e2884e6c62e8de47fe194eb74385906598fac42a
rostty <rostikmironov94@gmail.com>
2024-12-09 13:12:12 -0500

    .

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
     # Load animated sprite for block "6"
     # Assuming ARW2DSprite.png is 5 frames of 64x64 each
     TEXTURES["6"] = AnimatedSprite("assets/img/blocks/ARW2DSprite2.png", 24, 24, num_frames=14, frame_delay=1.05)
-
+    TEXTURES["7"] = AnimatedSprite("assets/img/enemies/enemiSP.png", 204, 68, num_frames=3, frame_delay=1.05)
     # Create world and player
     world = World("assets/maps/map.txt")
     player_start_x, player_start_y, player_layer = world.player_start_pos
