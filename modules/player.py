41e6e392a3cd98e4300646cf35fd76e1ac8d482d
Masontigger <Masontigger@outlook.com>
2024-12-14 17:43:58 -0500

    sprite update

--- modules/player.py
+++ modules/player.py
@@ -17,19 +17,31 @@ class Player:
             print(f"Error loading player texture '{texture_path}': {e}")
             sys.exit()
 
+    def direction(self, direction):
+        if direction == "up":
+            self.texture = pygame.image.load("assets/img/blocks/player_back.png").convert_alpha()
+        elif direction == "down":
+            self.texture = pygame.image.load("assets/img/blocks/player_front.png").convert_alpha()
+        elif direction == "left":
+            self.texture = pygame.image.load("assets/img/blocks/player_left.png").convert_alpha()
+        elif direction == "right":
+            self.texture = pygame.image.load("assets/img/blocks/player_right.png").convert_alpha()
+        else:
+            self.texture = pygame.image.load("assets/img/blocks/player.png").convert_alpha()
     def move(self, dx, dy, dt, world):
-        new_x = self.grid_x + dx * self.speed * dt
-        new_y = self.grid_y + dy * self.speed * dt
+        new_x = self.grid_x + (dx + dy) * self.speed * dt / 2**0.5
+        new_y = self.grid_y + (dy - dx) * self.speed * dt / 2**0.5
 
         int_new_x = int(new_x)
         int_new_y = int(new_y)
 
         if (0 <= int_new_x < len(world.map_data[self.layer][0]) and
             0 <= int_new_y < len(world.map_data[self.layer]) and
-            world.map_data[self.layer][int_new_y][int_new_x] > 0):
+            world.map_data[self.layer][int_new_y][int_new_x] > 0 and
+            world.map_data[self.layer + 1][int_new_y][int_new_x] == 0):
             self.grid_x, self.grid_y = new_x, new_y
         else:
-            # Position blockedx
+            # Position blocked
             pass
 
     def jump(self, direction, world):
@@ -70,10 +82,11 @@ class Player:
     def collides_with_blocks(self, new_x, new_y, world):
         """Check for collision with blocks in the world map."""
         # Convert new coordinates to grid coordinates (assuming tile map is used)
+
         grid_x = new_x // BLOCK_SIZE
         grid_y = new_y // BLOCK_SIZE
         
         # Check if the new position would collide with a non-empty block
-        if world.map_data[grid_y][grid_x] != 0:  # Assuming 0 means no block
+        if world.map_data[grid_y][grid_x] == 0:  # Assuming 0 means no block
             return True
         return False
